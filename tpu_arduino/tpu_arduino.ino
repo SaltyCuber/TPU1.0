d2f80740dc2b383cc89829813786e437c8473e94
Tape Dispenser <59711006+SaltyCuber@users.noreply.github.com>
2021-11-18 14:27:36 -0800

    add branching instructions, change how getting imms works

--- tpu_arduino/tpu_arduino.ino
+++ tpu_arduino/tpu_arduino.ino
@@ -16,7 +16,7 @@ void setup() {
   DDRA = 255;
   DDRC = 255;
   DDRL = 0;
-  }
+}
 
 byte readMem(byte lower, byte upper) {
   DDRL = 0;
@@ -24,31 +24,40 @@ byte readMem(byte lower, byte upper) {
   PORTA = upper;
   byte data = PINL;
   return data;
+}
+
+byte getImm() {
+  if (::regs[6] == 0xFF) {
+    ::pmemBank++;
   }
+  ::regs[6]++;
+  return readMem(::regs[6],::pmemBank);
+}
+
 
 byte calcFlags(int x) {
   bool flags[] = {false,false,false,false,false,false,false,false};
   if (x > 0xFF) {
     flags[0] = true;
-    }
+  }
   if (x == 0) {
     flags[1] = true;
-    }
+  }
   if ((x & 0b10000000) > 0) {
     flags[2] = true;
-    }
+  }
   if ((x & 1) > 0) {
     flags[3] = true;
-    }
+  }
   byte out = 0;
   for (int i = 0; i <=7; i++) {
     if (flags[i] == true) {
       out++;
-      }
-    out = out << 1;
     }
-  return out;
+    out = out << 1;
   }
+  return out;
+}
 
 void onClock() {
   instrReg = readMem(++regs[6], pmemBank);
@@ -63,11 +72,11 @@ void onClock() {
     
     case 1:
       // ADC : acc = acc + imm + carry
-      imm = readMem(++regs[6], pmemBank);
+      imm = getImm();
       temp = regs[1] + imm;
       if (regs[7] & 0b10000000 > 0) {
         temp++;
-        }
+      }
       regs[7] = calcFlags(temp);
       regs[1] = byte(temp);
       break;
@@ -77,17 +86,17 @@ void onClock() {
       temp = regs[1] + regs[reg];
       if (regs[7] & 0b10000000 > 0) {
         temp++;
-        }
+      }
       regs[7] = calcFlags(temp);
       regs[1] = byte(temp);
       break;
     
     case 3:
       // SBB : acc = acc - imm - carry
-      temp = regs[1] + (~readMem(++regs[6], pmemBank));
+      temp = regs[1] + (~getImm());
       if (regs[7] & 0b10000000 == 0) {
         temp++;
-        }
+      }
       regs[7] = calcFlags(temp);
       regs[1] = byte(temp);
       break;
@@ -98,18 +107,18 @@ void onClock() {
       temp = regs[1] + (~regs[reg]);
       if (regs[7] & 0b10000000 == 0) {
         temp++;
-        }
+      }
       regs[7] = calcFlags(temp);
       regs[1] = byte(temp);
       break;
 
     case 5:
       // ROR : acc = carry > imm > carry
-      imm = readMem(++regs[6], pmemBank);
+      imm = getImm();
       oldCarry = regs[7] & 0b10000000;
       if ((imm >> 1 << 1) < imm) {
         temp1 = 0b10000000;
-        }
+      }
       temp = imm >> 1 + oldCarry;
       regs[7] = calcFlags(temp) | temp1;
       regs[1] = byte(temp);
@@ -120,7 +129,7 @@ void onClock() {
       oldCarry = regs[7] & 0b10000000;
       if ((regs[reg] >> 1 << 1) < regs[reg]) {
         temp1 = 0b10000000;
-        }
+      }
       temp = regs[reg] >> 1 + oldCarry;
       regs[7] = calcFlags(temp) | temp1;
       regs[reg] = byte(temp);
@@ -128,7 +137,7 @@ void onClock() {
     
     case 7:
       // XOR : acc = acc ^ imm
-      imm = readMem(++regs[6], pmemBank);
+      imm = getImm();
       regs[1] = byte(regs[1] ^ imm);
       regs[7] = calcFlags(regs[1]) | (regs[7] & 0b10000000); // don't affect carry flag
       break;
@@ -141,7 +150,7 @@ void onClock() {
 
     case 9:
       // OR : acc = acc | imm
-      imm = readMem(++regs[6], pmemBank);
+      imm = getImm();
       regs[1] = byte(regs[1] | imm);
       regs[7] = calcFlags(regs[1]) | (regs[7] & 0b10000000);
       break;
@@ -154,7 +163,7 @@ void onClock() {
 
     case 11:
       // AND : acc = acc & imm
-      imm = readMem(++regs[6],pmemBank);
+      imm = getImm();
       regs[1] = byte(regs[1] & imm);
       regs[7] = calcFlags(regs[1]) | (regs[7] & 0b10000000);
       break;
@@ -167,7 +176,7 @@ void onClock() {
 
     case 13:
       // MTA : acc = imm
-      regs[1] = readMem(++regs[6], pmemBank);
+      regs[1] = getImm();
       regs[7] = calcFlags(regs[1]) | (regs[7] & 0b10000000);
       break;
 
@@ -179,7 +188,7 @@ void onClock() {
 
     case 15:
       // MTR : reg = imm
-      regs[reg] = readMem(++regs[6],pmemBank);
+      regs[reg] = getImm();
       regs[7] = calcFlags(regs[reg]) | (regs[7] & 0b10000000);
       break;
 
@@ -191,34 +200,61 @@ void onClock() {
 
     case 17:
       // JRI : jump to 16 bit address (reg = upper word, imm = lower word)
+      regs[6] = getImm();
+      pmemBank = regs[reg];
       break;
 
     case 18:
       // JRI : jump to 16 bit address (imm = upper word, reg = lower word)
+      pmemBank = getImm();
+      regs[6] = regs[reg];
       break;
 
     case 19:
       // BRS : branch to imm if bit set
+      if ((regs[7] & (1<<(reg-1)) == 1) {
+        temp = pmemBank;
+        regs[6] = getImm();
+        pmemBank = temp;
+      }
       break;
 
     case 20:
       // BRS : branch to acc if bit set
+      if ((regs[7] & (1<<(reg-1)) == 1) {
+        temp = pmemBank;
+        regs[6] = regs[1];
+        pmemBank = temp;
+      }
       break;
 
     case 21:
       // BNS : branch to imm if bit not set
+      if ((regs[7] & (1<<(reg-1)) == 0) {
+        temp = pmemBank;
+        regs[6] = getImm();
+        pmemBank = temp;
+      }
       break;
 
     case 22:
       // BNS : branch to acc if bit not set
+      if ((regs[7] & (1<<(reg-1)) == 1) {
+        temp = pmemBank;
+        regs[6] = regs[1];
+        pmemBank = temp;
+      }
       break;
 
     case 23:
       // LDA : acc = vmem[imm]
+      imm = getImm();
+      regs[1] = readMem(imm, vmemBank);
       break;
 
     case 24:
       // LDA : acc = vmem[reg]
+      regs[1] = readMem(regs[reg], vmemBank);
       break;
 
     case 25:
@@ -252,5 +288,5 @@ void onClock() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
+
